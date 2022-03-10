9f924af18965f6f12cfd552b0af0cc6c7493b6ba
Aleksandra <a.haniok@gmail.com>
2022-03-10 09:45:26 +0000

    Link categories.html template

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,3 +7,13 @@ from taskmanager.models import Category, Task
 # this is our home page
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():  # these are Python functions, not route names
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])  # both methods needed as we'll be submitting the data to the database
+def add_category():
+    return render_template("add_category.html")
